9f0a94eb436ad751a9e8e3ad45a18b823722a227
samuel3wang <yuanyuan030303@gmail.com>
2023-02-07 15:30:39 +0800

    deploy

---
+++ psql/psql.go
@@ -0,0 +1,34 @@
+package psql
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(){
+	var err error
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// dsn := "host=127.0.0.1 user=samuel password=3336 dbname=blog port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil{
+		log.Fatal("something bad happened")
+		fmt.Println("wrong connection")
+	}
+	DB = db
+	fmt.Println("Connect success")
+}
+
