b4e48addf321ba0072aedcb57ec9d1a19eac03ab
samuel3wang <yuanyuan030303@gmail.com>
2023-02-07 15:41:07 +0800

    debug

--- psql/psql.go
+++ psql/psql.go
@@ -22,7 +22,7 @@ func ConnectDB(){
 	dbName := os.Getenv("DB_NAME")
 
 	// dsn := "host=127.0.0.1 user=samuel password=3336 dbname=blog port=5432 sslmode=disable TimeZone=Asia/Taipei"
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil{
 		log.Fatal("something bad happened")
